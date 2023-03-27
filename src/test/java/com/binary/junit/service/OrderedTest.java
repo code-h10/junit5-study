199b9c702c4cad7b52bfc58b840b8f4389a48cb7
hwang <ilyoung6016@gmail.com>
2023-03-27 17:09:48 +0900

    studying ordered annotation

---
+++ src/test/java/com/binary/junit/service/OrderedTest.java
@@ -0,0 +1,31 @@
+package com.binary.junit.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(SpringExtension.class)
+public class OrderedTest {
+
+    @Test
+    @Order(1)
+    public void firstTest() {
+        System.out.println("First Test");
+    }
+
+    @Test
+    @Order(2)
+    public void secondTest() {
+        System.out.println("Second Test");
+    }
+
+    @Test
+    @Order(3)
+    public void thirdTest() {
+        System.out.println("Third Test");
+    }
+}
